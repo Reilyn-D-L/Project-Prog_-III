93907290b2db91a0fd0ebf5c7ce1893645ff1077
Reilyn-D-L <reilyndelarosal@gmail.com>
2022-11-26 22:08:02 -0400

    updated

---
+++ SCRIPT.js
@@ -0,0 +1,11 @@
+//alert("Esta pagina es peligrosa!!!")
+//alert("Full part coming soon !")
+
+function color() {
+    var streak = document.getElementById("streak");
+    streak.style.backgroundColor = "#36474f";
+    streak.style.cursor = 'pointer';
+}
+function clr() {
+    streak.style.backgroundColor = "#2e3b41";
+}
